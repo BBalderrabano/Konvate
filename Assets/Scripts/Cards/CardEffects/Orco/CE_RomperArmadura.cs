e0e0a56b85a54b365b2bc4c80df57dcc9abff6ca
BBalderrabano <bmbalderrabano@gmail.com>
2020-11-23 15:26:47 -0300

    Implementado Romper Armadura
    Implementada la carta Romper Armadura para el Orco

---
+++ Assets/Scripts/Cards/CardEffects/Orco/CE_RomperArmadura.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "Card Effects/Orco/Romper Armadura")]
+public class CE_RomperArmadura : CardEffect
+{
+    public override void Execute()
+    {
+        List<Card> played_cards = GM.GetOpponentHolder(card.owner.photonId).playedCards;
+
+        List<Card> defense = new List<Card>();
+
+        defense.AddRange(played_cards.Where(a => a.HasTags(CardTag.DEFENSA)).ToList());
+        defense.AddRange(played_cards.Where(a => a.HasTags(CardTag.DEFENSA_SUPERIOR)).ToList());
+
+        foreach(Card selected in defense)
+        {
+            parentAction.PushAction(new A_Discard(selected.instanceId, selected.owner.photonId));
+        }
+    }
+}
