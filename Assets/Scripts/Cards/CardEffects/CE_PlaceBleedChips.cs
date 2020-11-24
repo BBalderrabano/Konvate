e360f2dbfa17a2f0118daade915886dfedf3e01a
BBalderrabano <bmbalderrabano@gmail.com>
2020-11-24 17:27:32 -0300

    Inicio de Mago y cambio grafico a Orco
    Se comienza a implementar el mazo del mago y se cambia el grafico para la carta Fuerza en Numeros del Orco

---
+++ Assets/Scripts/Cards/CardEffects/CE_PlaceBleedChips.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card Effects/Place Bleed Chip")]
+public class CE_PlaceBleedChips : CardEffect
+{
+    public int amount;
+
+    private PlayerHolder player;
+
+    public override void Execute()
+    {
+        base.Execute();
+
+        player = card.owner;
+
+        parentAction.LinkAnimation(GM.animationManager.PlaceChip(parentAction.actionId, player.photonId, card.instanceId, ChipType.BLEED, amount));
+    }
+}
